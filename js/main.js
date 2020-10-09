28b5b55c18d61a31f6368f12054d69965d70c154
jingHuangSu1996 <jingmultiplefive@gmail.com>
2020-10-09 22:36:33 +0800

    feature: d3 section final

--- js/main.js
+++ js/main.js
@@ -1,59 +1,194 @@
 const d3 = require('d3');
-const DEFAULT_DATA = [127, 61, 256, 71, 15, 23];
+console.log(colorbrewer);
+
+let DEFAULT_DATA = [
+  {
+    product: 'Apples',
+    tons: 10123,
+    type: 'fruit',
+  },
+  {
+    product: 'Bananas',
+    tons: 12000,
+    type: 'fruit',
+  },
+  {
+    product: 'Plums',
+    tons: 8441,
+    type: 'fruit',
+  },
+  {
+    product: 'Bread',
+    tons: 1911,
+    type: 'grocery',
+  },
+  {
+    product: 'Cereals',
+    tons: 1011,
+    type: 'grocery',
+  },
+  {
+    product: 'Beer',
+    tons: 9011,
+    type: 'alcohol',
+  },
+  {
+    product: 'Wine',
+    tons: 8041,
+    type: 'alcohol',
+  },
+];
+
+const types = ['grocery', 'fruit', 'alcohol'];
+
+const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
 var result;
 
-let execute = (myData = DEFAULT_DATA) => {
-  let svg = d3.select('svg');
-  // binding data using d3
-  let bars = svg.selectAll('.bars').data(myData);
+/**
+ *
+ * More Complex data
+ *
+ * 1. How to access data in objects
+ * 2. How to sort data
+ * 3. How to properly deal with padding in an SVG.
+ * 4. How to use a scale with bands to evenly space the bars
+ * 5. How to update a scale
+ * 6. How to ensure object consistency for transition
+ * with the key function of data mapping
+ */
+let svg = d3
+  .select('#chart')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr(
+    'transform',
+    `translate(${margin.left}, ${margin.top})`
+  );
 
-  // how do we handle new elements?
-  // we start with transparent gray bars of width 0
-  let newBars = bars
+let execute = (data = DEFAULT_DATA) => {
+  const textWidth = 80;
+  const animationDuration = 2000;
+  const max = d3.max(data, d => d.tons);
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(types)
+    .range(colorbrewer.Accent[4]);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, max])
+    .range([0, width - textWidth])
+    .nice();
+
+  const yScale = d3
+    .scaleBand()
+    .range([0, height])
+    .padding(0.1);
+
+  xAxis = d3.axisBottom();
+  xAxis.scale(xScale);
+
+  svg
+    .append('g')
+    .classed('axis', true)
+    .attr('transform', `translate(${textWidth},${height})`)
+    .call(xAxis);
+
+  // here we update the yscale and set the domain to `product`
+  yScale.domain(data.map(d => d.product));
+
+  let barGroups = svg
+    .selectAll('.barGroup')
+    /**
+     * here we tell D3 how to know which objects are the
+     * same thing between updates
+     */
+    .data(data, function (d) {
+      return d.product;
+    });
+
+  /**
+   * ===================
+   *       Enter
+   * ===================
+   */
+  let barGroupsEnter = barGroups
     .enter()
-    .append('rect')
-    .attr('x', 0)
-    .attr('y', (_, i) => i * 30 + 50)
-    .attr('width', 0)
-    .attr('height', 20)
-    .style('opacity', 0)
-    .classed('bars', true);
+    .append('g')
+    .classed('barGroup', true)
+    .attr('transform', d => {
+      console.log(`translate(0, ${yScale(d.product)})`);
+      return `translate(0, ${yScale(d.product)})`;
+    });
 
-  // how do we handle things that are removed?
-  // we increase opacity
-  bars
-    .exit()
-    .style('opacity', 1)
+  // entering with transparent text and rects of width 0
+  barGroupsEnter.append('text').attr('opacity', 0);
+  barGroupsEnter.append('rect').attr('width', 0);
+
+  // remove
+  barGroups.exit().remove();
+
+  barGroups = barGroups.merge(barGroupsEnter);
+
+  barGroups
+    .select('text')
+    .text(d => d.product)
+    .attr('x', textWidth - 10)
+    .attr('dy', yScale.bandwidth() / 2)
+    .attr('text-anchor', 'end')
+    .attr('alignment-baseline', 'middle')
     .transition()
-    .duration(3000)
-    .style('opacity', 0)
-    .remove();
+    .duration(animationDuration)
+    .attr('opacity', 1);
 
-  console.log(newBars, bars);
-
-  // we merge the new bars with existing one
-  bars = newBars.merge(bars);
-
-  console.log('----> after', bars);
-
-  // how do we handle updates?
-  // we transition towards a blue opaque bar with a data driven width
-  bars
+  barGroups
+    .select('rect')
+    .attr('x', textWidth)
+    .attr('height', yScale.bandwidth())
+    .style('fill', function (d) {
+      return colorScale(d.type);
+    })
     .transition()
-    .duration(3000)
-    .attr('x', 0)
-    .attr('y', (_, i) => i * 30 + 50)
-    .attr('width', d => d)
-    .attr('height', 20)
-    .style('fill', 'aliceblue')
-    .style('opacity', 1);
+    .duration(animationDuration)
+    .attr('width', function (d) {
+      return xScale(d.tons);
+    });
+
+  barGroups
+    .transition()
+    .duration(animationDuration)
+    .attr('transform', function (d) {
+      return `translate(0, ${yScale(d.product)})`;
+    });
 };
 
-d3.select('#next').on('click', () =>
-  execute([500, 100, 120])
-);
+d3.select('#sort-select').on('change', function (e) {
+  console.log(e);
+  let active = e.target.value;
+  DEFAULT_DATA = DEFAULT_DATA.sort(function (a, b) {
+    switch (active) {
+      case 'product':
+      // fall through to type
+      case 'type':
+        if (a[active] < b[active]) {
+          return -1;
+        } else if (a[active] > b[active]) {
+          return 1;
+        } else {
+          return 0;
+        }
 
-execute();
+      case 'weight':
+        return b.tons - a.tons;
 
+      default:
+        return b.tons - a.tons;
+    }
+  });
+  execute();
+});
 console.log(result);
