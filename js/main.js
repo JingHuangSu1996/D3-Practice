a199d074012ceb9dfefa4d42ef9f17c2f59b8640
jingHuangSu1996 <jingmultiplefive@gmail.com>
2020-10-09 16:06:39 +0800

    feature: d3 colorScale

--- js/main.js
+++ js/main.js
@@ -1,58 +1,103 @@
 const d3 = require('d3');
-const DEFAULT_DATA = [127, 61, 256, 71, 15, 23];
+const DEFAULT_DATA = [
+  0.3,
+  -0.8,
+  0.01,
+  -0.4,
+  1.2,
+  1.3,
+  -0.2,
+  -1.2,
+  0.82,
+  0.4,
+  -0.2,
+  0.3,
+];
 
 var result;
 
-let execute = (myData = DEFAULT_DATA) => {
-  let svg = d3.select('svg');
-  // binding data using d3
-  let bars = svg.selectAll('.bars').data(myData);
+const HEIGHT = 400;
+const WIDTH = 800;
+const PADDING = 25;
 
-  // how do we handle new elements?
-  // we start with transparent gray bars of width 0
-  let newBars = bars
+/**
+ * Axes
+ *
+ * Drawing Axes and legends is critical for data visualization
+ * All of our examples, with the exception of the labelled bar
+ * charts didn't really tell us about the abs values of the data
+ * However Drawing axes well can be a lot of work. Fortunately
+ * D3 has extnesive support for axes
+ *
+ *
+ * this worked, we create a new axes by calling d3.axisBottom()
+ *
+ * ant tell it its value range by passing the relevant scale
+ * We then append the scale to the svg with this call
+ * `svg.append('g').call(xAxis)`, Now we can see that it
+ * overlaps with our bar charts and that some of the number are cloase to the
+ * edge of svg, To fix this we have to style the chart and add some margin
+ *
+ */
+let execute = (myData = DEFAULT_DATA) => {
+  const svg = d3
+    .select('svg')
+    .attr('width', WIDTH + 2 * PADDING)
+    .attr('height', HEIGHT + 2 * PADDING);
+  svg
+    .append('rect')
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT)
+    .attr('transform', `translate(${PADDING}, ${PADDING})`)
+    .style('stroke', 'none')
+    .style('fill', '#eeeeee');
+
+  let spacing = HEIGHT / myData.length;
+
+  let min = d3.min(myData);
+  let max = d3.max(myData);
+
+  let xScale = d3
+    .scaleLinear()
+    .domain([min, max])
+    .range([0, 800])
+    .nice();
+
+  let colorScale = d3
+    .scaleLinear()
+    .domain([min, 0, max])
+    .range(['darkred', 'lightgray', 'aliceblue']);
+
+  // create a new axis that has the ticks and labels on the bottom
+  let xAxis = d3.axisBottom();
+
+  // assign the scale to the axis
+  xAxis.scale(xScale);
+
+  svg
+    .selectAll('rect')
+    .data(myData)
     .enter()
     .append('rect')
-    .attr('x', 0)
-    .attr('y', (_, i) => i * 30 + 50)
-    .attr('width', 0)
+    .attr('transform', `translate(${PADDING}, ${PADDING})`)
+    .attr('x', d => xScale(Math.min(0, d))) // start at d if d is negative, else start at 0
+    .attr('y', (_, i) => i * spacing + 5)
+    .attr('width', d => {
+      console.log(xScale(d), xScale(0));
+      return Math.abs(xScale(d) - xScale(0));
+    })
     .attr('height', 20)
-    .style('opacity', 0)
-    .classed('bars', true);
+    .style('fill', d => colorScale(d));
 
-  // how do we handle things that are removed?
-  // we increase opacity
-  bars
-    .exit()
-    .style('opacity', 1)
-    .transition()
-    .duration(3000)
-    .style('opacity', 0)
-    .remove();
-
-  console.log(newBars, bars);
-
-  // we merge the new bars with existing one
-  bars = newBars.merge(bars);
-
-  console.log('----> after', bars);
-
-  // how do we handle updates?
-  // we transition towards a blue opaque bar with a data driven width
-  bars
-    .transition()
-    .duration(3000)
-    .attr('x', 0)
-    .attr('y', (_, i) => i * 30 + 50)
-    .attr('width', d => d)
-    .attr('height', 20)
-    .style('fill', 'aliceblue')
-    .style('opacity', 1);
+  svg
+    .append('g')
+    .classed('axis', true)
+    .attr(
+      'transform',
+      `translate(${PADDING}, ${HEIGHT + PADDING})`
+    )
+    .call(xAxis);
 };
-
-d3.select('#next').on('click', () =>
-  execute([500, 100, 120])
-);
 
 execute();
 
